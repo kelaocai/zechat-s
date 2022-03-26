8ae7ebf676976ff8b2003f90b28225765d6b1c63
kelaocai <kelaocai@163.com>
2022-03-26 23:18:44 +0800

    修改.gitignore

--- test.js
+++
@@ -1,6 +0,0 @@
-var dayjs= require('dayjs')
-dayjs().format()
-
-// console.log(dayjs.unix(1648049128).format('YYYY-MM-DD HH:mm:ss'))
-// console.log(dayjs().unix())
-console.log(dayjs().daysInMonth)
