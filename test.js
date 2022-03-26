89a88a70eb5e92351974cb292b18a72e6db94340
kelaocai <kelaocai@163.com>
2022-03-26 23:14:59 +0800

    添加依赖包

---
+++ test.js
@@ -0,0 +1,6 @@
+var dayjs= require('dayjs')
+dayjs().format()
+
+// console.log(dayjs.unix(1648049128).format('YYYY-MM-DD HH:mm:ss'))
+// console.log(dayjs().unix())
+console.log(dayjs().daysInMonth)
