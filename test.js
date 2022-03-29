158fd1080818702e1c5b71639a38ef8cf80a7e12
kelaocai <kelaocai@163.com>
2022-03-29 15:32:10 +0800

    修改首页,增加备案号

--- test.js
+++ test.js
@@ -4,3 +4,16 @@ dayjs().format()
 // console.log(dayjs.unix(1648049128).format('YYYY-MM-DD HH:mm:ss'))
 // console.log(dayjs().unix())
 console.log(dayjs().daysInMonth)
+
+
+
+var change=function (a) {
+
+    a=100
+    console.log('a:',a)
+    
+}
+
+var a=10
+change(a)
+console.log(a)
