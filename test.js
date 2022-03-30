a96fe776b03c451226d390094e857aad95f01268
kelaocai <kelaocai@163.com>
2022-03-30 17:29:40 +0800

    修改备案号

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-var dayjs= require('dayjs')
-dayjs().format()
+// var dayjs= require('dayjs')
+// dayjs().format()
 
-// console.log(dayjs.unix(1648049128).format('YYYY-MM-DD HH:mm:ss'))
-// console.log(dayjs().unix())
-console.log(dayjs().daysInMonth)
+// // console.log(dayjs.unix(1648049128).format('YYYY-MM-DD HH:mm:ss'))
+// // console.log(dayjs().unix())
+// console.log(dayjs().daysInMonth)
 
 
 
